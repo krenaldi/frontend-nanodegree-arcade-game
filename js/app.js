5fbb782b7c458fe1c2b570c682ea1988eb5977b2
Kris Renaldi <krenaldi@comcast.net>
2015-04-18 13:13:35 -0400

    Added variables to Enemy function
    Not sure if I needed to define a loc variable since loc is not being
    passed to this function

--- js/app.js
+++ js/app.js
@@ -2,7 +2,9 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this = Object.create(Enemy.prototype);
+    this.loc = loc;
+    return this;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +16,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.loc++;
 }
 
 // Draw the enemy on the screen, required method for game
