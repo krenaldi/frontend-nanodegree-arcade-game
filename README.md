72cc5de141080e04716e88fdc141240369ef5b5f
Kris Renaldi <krenaldi@comcast.net>
2015-04-26 15:52:35 -0400

    Modified ReadMe file and added resources

--- README.md
+++ README.md
@@ -4,3 +4,16 @@ frontend-nanodegree-arcade-game
 Students should use this rubric: https://www.udacity.com/course/viewer#!/c-ud015/l-3072058665/m-3072588797
 
 for self-checking their submission.
+
+Udacity Frogger Rules
+=====================
+Player must use the arrow keys on the keyboard to move their icon across the street to the water to reach the next level and score 1 point while avoiding the bugs moving across the screen. The bugs will increase in speed as the levels progress.
+
+If the player makes contact with a bug, the game resets and the player loses 1 life.
+
+Once the player reaches level 25, an alert pops up stating that the play beat the game and the game is over.
+
+If the player loses all 3 lives, an alert pops up stating that the game is over.
+
+In order to play again, the player must refresh the page and click on OK in the alert box to restart the game.
+
--- js/app.js
+++ js/app.js
@@ -10,11 +10,11 @@ rowArray = [56, 139, 222]; // array of y values for each row of bug
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
-}
+};
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -24,18 +24,18 @@ Enemy.prototype.update = function(dt) {
     // all computers.
     this.x += 230 * this.speed * dt; //determines speed
     //if bug hits edge of canvas it resets back to the begninning at different speed
-    if (this.x > 505){
+    if (this.x > 505) {
         this.x = -100;
         var row = rowArray[Math.floor(Math.random() * rowArray.length)];
         this.y = row;
         this.speed = (Math.random() * (maxSpeed - minSpeed) + minSpeed);
     }
-}
+};
 
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
+};
 
 // Now write your own player class
 // This class requires an update(), render() and
@@ -50,43 +50,36 @@ var Player = function() {
 };
 
 // resets Player to starting position
-var resetPlayer = function(player){
-    player.x = (ctx.canvas.width / 2) - (101/2);
+var resetPlayer = function(player) {
+    player.x = (ctx.canvas.width / 2) - (101 / 2);
     player.y = 388;
-}
-
-/*var GameOver = function(player){
-    ctx.text = "GAME OVER!!";
-    ctx.textSub = "Refresh Page to Try Again";
-    ctx.textColor = "red";
-    this.draw();
-}*/
+};
 
 // this function is called when player dies by hitting a bug
-function playerDies(player){
+function playerDies(player) {
     //alert("You died. Click OK to continue.");
     resetPlayer(player);
     player.lives -= 1;
     document.getElementById("lives").innerHTML = player.lives;
-    // pulls up a game over screen when lives equals 0
-    if (player.lives == 0){
-        alert("GAME OVER! Refresh the page and click OK to play again.")
+    // pulls up a game over alert when lives equals 0
+    if (player.lives === 0) {
+        alert("GAME OVER! Refresh the page and click OK to play again.");
     }
 }
 
 //sets up collision detection & what happens when player reaches top row
 Player.prototype.update = function(dt) {
     // detects collision on each row & runs playerDies function when collision occurs
-    if ((Math.floor(allEnemies[0].x) < this.x + 35) && (Math.floor(allEnemies[0].x) > this.x - 35) && Math.floor(allEnemies[0].y) == this.y){
+    if ((Math.floor(allEnemies[0].x) < this.x + 35) && (Math.floor(allEnemies[0].x) > this.x - 35) && Math.floor(allEnemies[0].y) == this.y) {
         playerDies(player);
     }
-    if ((Math.floor(allEnemies[1].x) < this.x + 35) && (Math.floor(allEnemies[1].x) > this.x - 35) && Math.floor(allEnemies[1].y) == this.y){
+    if ((Math.floor(allEnemies[1].x) < this.x + 35) && (Math.floor(allEnemies[1].x) > this.x - 35) && Math.floor(allEnemies[1].y) == this.y) {
         playerDies(player);
     }
-    if ((Math.floor(allEnemies[2].x) < this.x + 35) && (Math.floor(allEnemies[2].x) > this.x - 35) && Math.floor(allEnemies[2].y) == this.y){
+    if ((Math.floor(allEnemies[2].x) < this.x + 35) && (Math.floor(allEnemies[2].x) > this.x - 35) && Math.floor(allEnemies[2].y) == this.y) {
         playerDies(player);
     }
-    if (this.y == -27){
+    if (this.y == -27) {
         levelUp();
     }
 };
@@ -101,7 +94,7 @@ Player.prototype.handleInput = function(key) {
     switch (key) {
 
         case 'left':
-            if (player.x <= 0){
+            if (player.x <= 0) {
                 player.x = 0;
             } else {
                 player.x -= 101;
@@ -109,7 +102,7 @@ Player.prototype.handleInput = function(key) {
             break;
 
         case 'right':
-            if (player.x >= 400){
+            if (player.x >= 400) {
                 player.x = 404;
             } else {
                 player.x += 101;
@@ -117,7 +110,7 @@ Player.prototype.handleInput = function(key) {
             break;
 
         case 'up':
-            if (player.y <= -22){
+            if (player.y <= -22) {
                 player.y = -27;
             } else {
                 player.y -= 83;
@@ -125,7 +118,7 @@ Player.prototype.handleInput = function(key) {
             break;
 
         case 'down':
-            if (player.y >= 388){
+            if (player.y >= 388) {
                 player.y = 388;
             } else {
                 player.y += 83;
@@ -160,26 +153,24 @@ enemy3.speed = 1;
 
 // Place the player object in a variable called player
 var player = new Player();
-//player.x = (ctx.canvas.width / 2) - (101/2);
-//player.y = 388;
 player.lives = 3; // player starts with 3 lives
 player.score = 0; // player score starts with 0 score
 
 // function is called each time the player reaches the water & goes to the next level
-function levelUp(){
-    level += 1;
-    /*if (level == 5){
-        alert("You win!");
-    }*/
-    score += 1;
-    document.getElementById("level").innerHTML = level;
-    document.getElementById("score").innerHTML = score;
-    resetPlayer(player);
-    minSpeed += 0.3;
-    maxSpeed += 0.3;
-}
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+function levelUp() {
+        level += 1;
+        if (level == 25) {
+            alert("CONGRATUALTIONS!!! You win! Refresh the page and click on OK to play again.");
+        }
+        score += 1;
+        document.getElementById("level").innerHTML = level;
+        document.getElementById("score").innerHTML = score;
+        resetPlayer(player);
+        minSpeed += 0.3;
+        maxSpeed += 0.3;
+    }
+    // This listens for key presses and sends the keys to your
+    // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
